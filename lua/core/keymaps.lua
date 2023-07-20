2f1bf93ed4ecd4617818681819a38680e4ffde6d
Guillermo Amarante <gac317@gmail.com>
2023-07-20 10:29:00 +0800

    Complete settings from tutorial up to autocomplete

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,8 @@
+vim.opt.cursorline = true
+vim.opt.showcmd = true
+
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
