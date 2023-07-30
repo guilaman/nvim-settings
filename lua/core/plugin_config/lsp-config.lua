b598057a11b7ea8db21d240866038df27caa5b75
Guillermo Amarante <gac317@gmail.com>
2023-07-30 08:00:12 +0800

    Add LSP languages html and css

--- lua/core/plugin_config/lsp-config.lua
+++ lua/core/plugin_config/lsp-config.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls", "pylsp" }
+  ensure_installed = { "lua_ls", "pylsp", "html", "cssls" }
 })
 
 local on_attach = function(_, _)
