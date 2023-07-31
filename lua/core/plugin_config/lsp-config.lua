77196de29dcef49c192e0f9b5076e834e4dc918a
Guillermo Amarante <gac317@gmail.com>
2023-07-31 08:12:33 +0800

    Fix autocompletion by requiring it for all languages

--- lua/core/plugin_config/lsp-config.lua
+++ lua/core/plugin_config/lsp-config.lua
@@ -15,7 +15,22 @@ end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-require("lspconfig").sumneko_lua.setup {
+require("lspconfig").lua_ls.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+require("lspconfig").pylsp.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+require("lspconfig").html.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+require("lspconfig").cssls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
 }
